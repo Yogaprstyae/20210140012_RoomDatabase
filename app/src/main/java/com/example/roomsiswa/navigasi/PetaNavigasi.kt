05bca69d99dc1ca6f621eb873074fa398ccff460
Yogaprstyae <m.yoga.ft21@mail.umy.ac.id>
2023-12-06 02:45:07 +0700

    Membuat fun HostNavigasi beserta isinya

--- app/src/main/java/com/example/roomsiswa/navigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/roomsiswa/navigasi/PetaNavigasi.kt
@@ -11,9 +11,14 @@ import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.roomsiswa.R
+import com.example.roomsiswa.ui.theme.halaman.DestinasiEntry
+import com.example.roomsiswa.ui.theme.halaman.DestinasiHome
+import com.example.roomsiswa.ui.theme.halaman.EntrySiswaScreen
+import com.example.roomsiswa.ui.theme.halaman.HomeScreen
 
 
 @Composable
@@ -44,6 +49,23 @@ fun SiswaTopAppBar(
             }
         }
     )
+}
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(navController = navController, startDestination = DestinasiHome.route, modifier = Modifier)
+    {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+            )
+        }
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(navigateBack = { navController.popBackStack() })
+        }
+    }
 }
 
 
