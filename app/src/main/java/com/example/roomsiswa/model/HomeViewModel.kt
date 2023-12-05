37c48c2a30b83878b3b410316ac6933cabfe6e59
Yogaprstyae <m.yoga.ft21@mail.umy.ac.id>
2023-12-05 15:13:06 +0700

    Membuat class HomeViewModel

--- app/src/main/java/com/example/roomsiswa/model/HomeViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/HomeViewModel.kt
@@ -1,2 +1,10 @@
 package com.example.roomsiswa.model
 
+import androidx.lifecycle.ViewModel
+import com.example.roomsiswa.repository.RepositorySiswa
+
+class HomeViewModel (private val repositorySiswa: RepositorySiswa): ViewModel() {
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+}
