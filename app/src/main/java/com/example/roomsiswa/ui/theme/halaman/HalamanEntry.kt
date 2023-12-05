8bdf126185a2d56bb8575c10c404aed8fd7db84a
Yogaprstyae <m.yoga.ft21@mail.umy.ac.id>
2023-12-06 00:18:28 +0700

    Membuat object DestinasiEntry

--- app/src/main/java/com/example/roomsiswa/ui/theme/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/roomsiswa/ui/theme/halaman/HalamanEntry.kt
@@ -19,7 +19,12 @@ import androidx.compose.ui.text.input.KeyboardType
 import com.example.roomsiswa.R
 import com.example.roomsiswa.model.DetailSiswa
 import com.example.roomsiswa.model.UIStateSiswa
+import com.example.roomsiswa.navigasi.DestinasiNavigasi
 
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
 @Composable
 fun EntrySiswaBody(
     uiStateSiswa: UIStateSiswa,
