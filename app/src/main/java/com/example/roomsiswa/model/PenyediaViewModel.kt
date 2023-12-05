79ec948c043fc7d93ecb0b101ab99a294d42700a
Yogaprstyae <m.yoga.ft21@mail.umy.ac.id>
2023-12-05 21:26:33 +0700

    Membuat obejct PenyediaViewModel

--- app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
@@ -1,2 +1,15 @@
 package com.example.roomsiswa.model
 
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositorySiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositorySiswa)
+        }
+    }
+}
