0c8984119b9ce3fdd7b2d1f8643deb968573df44
Yogaprstyae <m.yoga.ft21@mail.umy.ac.id>
2023-12-05 22:37:32 +0700

    Membuat column untuk outlinetext

--- app/src/main/java/com/example/roomsiswa/ui/theme/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/roomsiswa/ui/theme/halaman/HalamanEntry.kt
@@ -1,7 +1,11 @@
 package com.example.roomsiswa.ui.theme.halaman
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import com.example.roomsiswa.R
 import com.example.roomsiswa.model.DetailSiswa
 
 @Composable
@@ -10,4 +14,11 @@ fun FormInputSiswa(
     modifier: Modifier = Modifier,
     onValueChange: (DetailSiswa) -> Unit = {},
     enable: Boolean = true
-){}
+){
+    Column (
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+    ){
+
+    }
+}
