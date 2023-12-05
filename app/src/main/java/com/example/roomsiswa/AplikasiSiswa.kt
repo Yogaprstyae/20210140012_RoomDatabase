e04db13b2148964b069270caaaf18da25c5ba8c7
Yogaprstyae <m.yoga.ft21@mail.umy.ac.id>
2023-12-05 21:14:03 +0700

    Menambah isi class AplikasiSiswa

--- app/src/main/java/com/example/roomsiswa/AplikasiSiswa.kt
+++ app/src/main/java/com/example/roomsiswa/AplikasiSiswa.kt
@@ -1,4 +1,17 @@
 package com.example.roomsiswa
 
-class AplikasiSiswa {
+import android.app.Application
+import com.example.roomsiswa.repository.ContainerApp
+import com.example.roomsiswa.repository.ContainerDataApp
+
+class AplikasiSiswa : Application() {
+    /**
+     * AppContainer instance digunakan oleh kelas" lainnya untuk mendapatkan dependensi
+     */
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
 }
