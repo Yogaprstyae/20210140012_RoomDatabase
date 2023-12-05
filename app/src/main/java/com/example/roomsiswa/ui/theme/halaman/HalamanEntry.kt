9fc983962de26585db9bb855348397af70dc7b16
Yogaprstyae <m.yoga.ft21@mail.umy.ac.id>
2023-12-05 22:34:48 +0700

    Membuat fungi FormInputSiswa

--- app/src/main/java/com/example/roomsiswa/ui/theme/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/roomsiswa/ui/theme/halaman/HalamanEntry.kt
@@ -1,2 +1,13 @@
 package com.example.roomsiswa.ui.theme.halaman
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.roomsiswa.model.DetailSiswa
+
+@Composable
+fun FormInputSiswa(
+    detailSiswa: DetailSiswa,
+    modifier: Modifier = Modifier,
+    onValueChange: (DetailSiswa) -> Unit = {},
+    enable: Boolean = true
+){}
