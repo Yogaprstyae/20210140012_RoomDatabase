f85c8822b4e208633a67d2917462d7b8c501eaa5
Yogaprstyae <m.yoga.ft21@mail.umy.ac.id>
2023-12-05 12:49:37 +0700

    Membuat class EntryViewModel dan var uiStateSiswa

--- app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
@@ -1,2 +1,15 @@
 package com.example.roomsiswa.model
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.roomsiswa.repository.RepositorySiswa
+
+class EntryViewModel(private val repositorySiswa: RepositorySiswa): ViewModel(){
+    /**
+     * Berisi status Siswa saat ini
+     */
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+}
