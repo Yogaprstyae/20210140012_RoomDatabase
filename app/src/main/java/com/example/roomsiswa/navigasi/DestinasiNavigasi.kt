07999cce8f55172024c01015e0334a16ee71d469
Yogaprstyae <m.yoga.ft21@mail.umy.ac.id>
2023-12-05 22:25:27 +0700

    Menambah isi class DestinasiNavigasi

--- app/src/main/java/com/example/roomsiswa/navigasi/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/roomsiswa/navigasi/DestinasiNavigasi.kt
@@ -1,4 +1,12 @@
 package com.example.roomsiswa.navigasi
 
 interface DestinasiNavigasi {
+    /**
+     * Nama unik untuk menentukan jalaur untuk composable
+     */
+    val route: String
+    /**
+     * String resource id yang berisi judul yang akan ditampilkan di layar halaman
+     */
+    val titleRes: Int
 }
