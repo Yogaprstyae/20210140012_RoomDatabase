fead2cbbf0bcea2e95121ea3fae177f64eb3ea15
Yogaprstyae <m.yoga.ft21@mail.umy.ac.id>
2023-12-05 14:03:10 +0700

    Membuat data class DetailSiswa

--- app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
@@ -26,3 +26,9 @@ data class UIStateSiswa(
     val detailSiswa: DetailSiswa = DetailSiswa(),
     val inEntryValid: Boolean = false
 )
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = "",
+)
